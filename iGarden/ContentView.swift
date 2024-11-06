202c3c18530ae8997643719b67ce9770fae5ebc3
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-11-05 16:05:18 -0800

    Initial Commit

---
+++ iGarden/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iGarden
+//
+//  Created by Nikolai Nobadi on 11/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iGarden/iGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  iGardenApp.swift
+//  iGarden
+//
+//  Created by Nikolai Nobadi on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct iGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
